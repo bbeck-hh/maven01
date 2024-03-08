240fcc8c2d3557bb478ef112427d1410a98a001c
Bjoern Beck <beck.b.hh@gmail.com>
2024-03-08 15:09:04 +0100

    Initial commit

---
+++ src/main/java/de_neuefische/Main.java
@@ -0,0 +1,18 @@
+package de_neuefische;
+
+public class Main {
+    public static void main(){
+        System.out.println("Hello World");
+    }
+
+    public static int calcSum(int a, int b) {
+        return a+b;
+    }
+
+    public static boolean isEven(int a) {
+        if (a%2 == 0){
+            return true;
+        }
+        return false;
+    }
+}
