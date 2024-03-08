ba7b2a499345b0b847c08af3d798183b53e83da7
Bjoern Beck <beck.b.hh@gmail.com>
2024-03-08 17:04:43 +0100

    Added new unit test for bonus-> challange

--- src/main/java/de_neuefische/Main.java
+++ src/main/java/de_neuefische/Main.java
@@ -20,14 +20,34 @@ public class Main {
         return false;
     }
 
-    public static String convertToUpperCase (String inputString){
+    public static String convertToUpperCase(String inputString) {
         return inputString.toUpperCase();
     }
 
-    public static boolean isPositiveNumber (int a){
-        if (a%2 == 0){
+    public static boolean isPositiveNumber(int a) {
+        if (a > 0) {
             return true;
         }
+        return false;
+    }
+
+    // Task link: https://www.codewars.com/kata/56a4addbfd4a55694100001f/javascript
+    public static boolean checkIfSheSaidHallo(String text) {
+        String insensitiveText = text.toLowerCase();
+        String ignoreForeignCharacters = insensitiveText.replaceAll("[^a-zA-Z\\d\\s:]", " ");
+        System.out.println(ignoreForeignCharacters);
+        String[] convertedTextToArray = ignoreForeignCharacters.split(" ");
+
+        String[] possiblePeopleHelloes = {"hello", "ciao", "salut", "hallo", "hola", "ahoj", "czesc"};
+
+        for (String arrayText : convertedTextToArray) {
+            for (String possibleHello : possiblePeopleHelloes) {
+                if (arrayText.equals(possibleHello)) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 
